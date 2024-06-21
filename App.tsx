d178cfffcf046c9e99acc581b5e6c141b5630e61
Азизжон <akhmedov_0808@mail.ru>
2024-06-21 09:37:02 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,161 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import axios from 'axios';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+function App(): React.JSX.Element {
+  const [checkStarted, setCheckStarted] = useState(false);
+  const [checkStartCount, setCheckStartCount] = useState(0);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const [activeOrderStarted, setActiveOrderStarted] = useState(false);
+  const [activeOrderStartCount, setActiveOrderStart] = useState(0);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  function matchRatio(s1, s2) {
+    const dp = Array(s1.length + 1)
+      .fill(0)
+      .map(() => Array(s2.length + 1).fill(0));
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    for (let i = 1; i <= s1.length; i++) {
+      for (let j = 1; j <= s2.length; j++) {
+        if (s1[i - 1] === s2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    const lcsLength = dp[s1.length][s2.length];
+    const ratio = (lcsLength / Math.max(s1.length, s2.length)) * 100;
+    return ratio.toFixed(2);
+  }
+
+  const getCars = async () => {
+    const {data} = await axios({
+      method: 'post',
+      url: 'https://savdo.uzavtosanoat.uz/b/ap/stream/ph&models_all_v2',
+      data: {
+        filial_id: 100,
+        is_web: 'Y',
+      },
+    });
+    if (
+      data.filter(
+        item =>
+          matchRatio(item?.name.toLowerCase(), 'cobalt') > 55 ||
+          matchRatio(item?.name.toLowerCase(), 'lacetti') > 55,
+      ).length
+    ) {
+      console.log('123');
+
+      const {data: test} = await axios({
+        method: 'post',
+        url: 'https://umidbeku.jprq.app/api/v1/shop/check',
+        data: {data},
+      });
+      console.log(test);
+    }
+  };
+
+  const handleActiveOrder = async activeOrder => {
+    const url = `https://savdo.uzavtosanoat.uz/t/ap/runorder?mod=${activeOrder.model_id}&token=${activeOrder.person.rcode}`;
+
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log('handleActiveOrder', data);
+    if (data.status === 1 || data.status === '1') {
+      console.log('success');
+      await axios({
+        method: 'put',
+        url: `https://umidbeku.jprq.app/api/v1/shop/active-orders/${activeOrder.id}`,
+        data: {
+          is_created: true,
+          is_active: false,
+        },
+      });
+    }
+  };
+
+  const sendCarRequests = () => {
+    console.log('start', checkStarted);
+    const promise = new Array(5).fill(null).map(qq => getCars());
+    Promise.all(promise).finally(() => {
+      console.log('end');
+      setTimeout(() => setCheckStartCount(checkStartCount + 1), 5000);
+    });
+  };
+
+  const sendActiveOrdersRequest = async () => {
+    console.log('start', checkStarted);
+    const {data: activeOrders} = await axios({
+      method: 'get',
+      url: 'https://umidbeku.jprq.app/api/v1/shop/active-orders',
+    });
+    console.log(activeOrders);
+    const promise = activeOrders.map(item => handleActiveOrder(item));
+    Promise.all(promise).finally(() => {
+      console.log('end');
+      setTimeout(() => setActiveOrderStart(activeOrderStartCount + 1), 5000);
+    });
+  };
+
+  useEffect(() => {
+    if (checkStarted) {
+      sendCarRequests();
+    }
+  }, [checkStartCount, checkStarted]);
+
+  useEffect(() => {
+    if (activeOrderStarted) {
+      sendActiveOrdersRequest();
+    }
+  }, [activeOrderStarted, activeOrderStartCount]);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 300,
+          height: 40,
+          borderRadius: 8,
+          backgroundColor: checkStarted ? 'red' : 'green',
+        }}
+        onPress={() => setCheckStarted(!checkStarted)}>
+        <Text style={{color: 'white', fontSize: 18}}>
+          {checkStarted ? 'Stop check' : 'Start check'}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 300,
+          height: 40,
+          borderRadius: 8,
+          backgroundColor: activeOrderStarted ? 'red' : 'green',
+          marginTop: 40,
+        }}
+        onPress={() => setActiveOrderStarted(!activeOrderStarted)}>
+        <Text style={{color: 'white', fontSize: 18}}>
+          {activeOrderStarted ? 'Stop active order' : 'Start active order'}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
 
